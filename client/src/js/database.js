7b5209d90a98c692f11958158eeecd04686e2d88
DillenGrant <dillen.grant72@gmail.com>
2024-01-16 21:14:09 -0700

    source js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,47 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await openDB(DB_NAME, DB_VERSION);
+    // Create a connection to the database database and version we want to use.
+    // const jateDB = await openDB('jate', 1);
+    // Create a new transaction and specify the database and data privileges.
+    const tx = db.transaction(DB_STORE_NAME, 'readwrite');
+
+    // Open up the desired object store.
+    const store = tx.objectStore(DB_STORE_NAME);
+    // Put requires an object as an argument.
+    await store.put({value: content, id: 1});
+    tx.oncomplete;
+    // Get confirmation of the request.
+    console.log('Data added to the database', content);
+  } catch (error) {
+    console.error('Error adding data to the database', error);
+    throw error;
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const db = await openDB(DB_NAME, DB_VERSION);
+    // Create a connection to the database database and version we want to use.
+    // const jateDB = await openDB('jate', 1);
+    // Create a new transaction and specify the database and data privileges.
+    const tx = db.transaction(DB_STORE_NAME, 'readonly');
+
+    // Open up the desired object store.
+    const store = tx.objectStore(DB_STORE_NAME);
+    const result = await store.get(1);
+    console.log('result', result);
+    // Get confirmation of the request.
+    console.log('result.value', result.value);
+    return result.value;
+  } catch (error) {
+    console.error('Error getting data from the database', error);
+    throw error;
+  }
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,28 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    deferredPrompt = event;
+    btnInstall.classList.remove('hidden');
+  });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+btnInstall.addEventListener('click', async () => {
+    if (!deferredPrompt) {
+      return;
+    }
+    deferredPrompt.prompt();
+    
+    const { outcome } = await deferredPrompt.userChoice;
+    console.log(`User response to the install prompt: ${outcome}`);
+  
+    deferredPrompt = null;
+  
+    btnInstall.classList.add('hidden');
+  });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('App install success');
+  });
